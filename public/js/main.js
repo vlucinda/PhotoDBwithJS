448a64bc0a6a291e5200588ab39e7069573fd18c
vlucinda <vlucinda@yahoo.com>
2017-07-18 20:36:12 -0400

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,30 @@
 //main.js
+/*jshint esversion: 6 */
 "use strict";
 
 
 var update = document.getElementById('update');
 var del = document.getElementById('delete');
 
+
 //Fetch triggers PUT request
 /* globals fetch */
 
 update.addEventListener('click', function () {
-  fetch('books', {
+  fetch('/public/index.html', {
     method: 'put',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
-      'title': 'Darth Vader',
-      'autor': 'I find your lack of faith disturbing.'
+        'description' : 'Levant',
     });
   });
+    //fetch "ok" method to see if update worked
   .then(response => {
     if (response.ok) return response.json()
   });
   .then(data => {
     console.log(data);
+      window.location.reload(true);
   });
 });
 
@@ -32,9 +35,14 @@ del.addEventListener('click', function () {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      'title': 'Darth Vader'
+      'description': 'tldr'
     });
-  });.then(function (response) {
-    window.location.reload()
+  });
+   .then(res => {
+    if (res.ok) return res.json();
+  });
+  .then(data => {
+    console.log(data)
+    window.location.reload();
   });
 });
